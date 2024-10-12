a197885a8e2c0327ccfb7b2fe3a0d59d6bc2ccd0
Garik Asplund <garik.asplund@gmail.com>
2024-10-11 23:29:11 -0700

    rough draft

---
+++ src/lib/components/ui/button/OpenSign.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let isOpen: boolean;
+</script>
+
+<div class="flex-col items-center justify-center space-y-1.5">
+	<p class="text-xs text-center text-gray-200 font-extralight">11 am to 10 pm</p>
+	<div class="flex items-center justify-center gap-2">
+		<div
+			class={`
+        h-3 w-3 
+        rounded-full
+        ${
+					isOpen
+						? 'bg-green-500 shadow-sm shadow-green-400/70'
+						: 'bg-red-500 shadow-lg shadow-red-500/50'
+				}
+        transition-all duration-300
+      `}
+		></div>
+		<span class="text-lg text-gray-200 uppercase">{isOpen ? 'Open' : 'Closed'}</span>
+	</div>
+	<p class="text-xs text-center text-gray-200 font-extralight">Closed Sundays</p>
+</div>

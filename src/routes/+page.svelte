e6eb502ace27e0e872b755bbf14667f4798939ef
Garik Asplund <garik.asplund@gmail.com>
2024-10-12 21:55:59 -0700

    better spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,11 +18,16 @@
 
 <div class="absolute flex items-center justify-around w-full text-gray-200"></div>
 <nav
-	class="container relative z-10 flex flex-col-reverse items-center justify-center min-h-screen p-4 mx-auto text-2xl font-thin text-gray-200 bg-black/50 md:min-h-fit md:justify-between md:flex-row md:hidden"
+	class="container relative z-10 flex flex-col items-center justify-center min-h-screen p-4 mx-auto text-2xl font-thin text-gray-200 bg-black/50 md:min-h-fit md:justify-between md:flex-row md:hidden"
 >
-	<!-- Contact -->
-	<div class="m-7"><OpenSign bind:isOpen /></div>
-
+	<!-- Order now! -->
+	<button class="p-4 border-red-500 border rounded-sm hover:opacity-85 {isOpen ? '' : 'hidden'}"
+		><a href="https://www.toasttab.com/range-rider-107-northwest-1st-street/v3">Order now!</a
+		></button
+	><!-- Menu -->
+	<button class="p-4 hover:opacity-85"><a href="/menu">Menu</a></button>
+	<!-- Music -->
+	<button class="p-4 hover:opacity-85"><a href="/music">Music</a></button>
 	<!-- Phone number: clickable link on mobile, plain text on desktop -->
 	<div class="m-11">
 		{#if isMobile}
@@ -31,15 +36,6 @@
 			<span>(541) 426-2337</span>
 		{/if}
 	</div>
-
-	<!-- Music -->
-	<button class="p-2 hover:opacity-85"><a href="/music">Music</a></button>
-	<!-- Menu -->
-	<button class="p-2 hover:opacity-85"><a href="/menu">Menu</a></button>
-
-	<!-- Order now! -->
-	<button class="p-2 border-red-500 border rounded-sm hover:opacity-85 {isOpen ? '' : 'hidden'}"
-		><a href="https://www.toasttab.com/range-rider-107-northwest-1st-street/v3">Order now!</a
-		></button
-	>
+	<!-- Contact -->
+	<div class="m-7"><OpenSign bind:isOpen /></div>
 </nav>

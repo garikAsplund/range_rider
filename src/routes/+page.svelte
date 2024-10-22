c4cca6c56cc001a0d62a9a78bcbe5a2246beced7
Garik Asplund <garik.asplund@gmail.com>
2024-10-22 12:00:29 -0700

    mobile buttons full width, better practices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <nav
-	class="container relative z-10 flex flex-col items-center justify-center min-h-screen p-4 mx-auto text-2xl font-thin text-gray-200 bg-black/70 md:hidden"
+	class="container relative z-10 flex flex-col items-center justify-center w-full min-h-screen p-4 mx-auto text-2xl font-thin text-gray-200 bg-black/70 md:hidden"
 	aria-label="Main navigation on mobile devices"
 >
 	<!-- Open sign -->
@@ -21,37 +21,38 @@
 	</div>
 
 	<!-- Music -->
-	<button class="p-6 hover:opacity-85"><a href="/music" aria-label="Music">Music</a></button>
+	<a href="/music" aria-label="Music" class="block w-full p-6 text-center hover:opacity-85">
+		Music
+	</a>
 
 	<!-- Menu -->
-	<button class="p-6 hover:opacity-85"><a href="/menu" aria-label="Menu">Menu</a></button>
+	<a href="/menu" aria-label="Menu" class="block w-full p-6 text-center hover:opacity-85"> Menu </a>
 
-	<!-- Order now! -->
-	<button
-		class="p-6 border-red-500 border rounded-sm hover:opacity-85 {isOpen ? '' : 'hidden'}"
+	<a
+		href="https://www.toasttab.com/range-rider-107-northwest-1st-street/v3"
+		class=" w-full flex justify-center hover:opacity-85 {isOpen ? '' : 'hidden'}"
 		aria-hidden={!isOpen}
-		><a href="https://www.toasttab.com/range-rider-107-northwest-1st-street/v3">Order now!</a
-		></button
 	>
-	
+		<div class="p-6 text-center border border-red-500 rounded-sm w-fit">Order now!</div>
+	</a>
+
 	<!-- Directions -->
-	<button class="p-6 hover:opacity-85 md:hidden"
-		><a
-			href="https://www.google.com/maps/dir//107%20NW%201st%20St,%20Enterprise,%20Oregon%2097828"
-			target="_blank">Directions</a
-		></button
+	<a
+		href="https://www.google.com/maps/dir//107%20NW%201st%20St,%20Enterprise,%20Oregon%2097828"
+		target="_blank"
+		class="block w-full p-6 text-center hover:opacity-85 md:hidden"
 	>
+		Directions
+	</a>
 
 	<!-- Phone number: clickable link on mobile, plain text on desktop -->
-	<div class="p-6">
-		{#if isMobile}
-			<a href="tel:541-426-2337" class="text-xl" aria-label="Call Range Rider at 541-426-2337"
-				>(541) 426-2337</a
-			>
-		{:else}
-			<span aria-label="Call Range Rider at 541-426-2337">(541) 426-2337</span>
-		{/if}
-	</div>
+	<a
+		href={isMobile ? 'tel:541-426-2337' : '#'}
+		class="block w-full p-6 text-center hover:opacity-85"
+		aria-label="Call Range Rider at 541-426-2337"
+	>
+		<div class="text-xl">(541) 426-2337</div>
+	</a>
 
 	<!-- Hours when closed -->
 	<!-- <div class="absolute inset-x-0 bottom-0 mb-6">
--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,23 +1,57 @@
-<!-- Specials -->
-<button class="hidden p-6 m-6 -translate-y-12 md:block hover:text-gray-300">
-	<a
-		href="menu/specials"
-		class="text-3xl font-thin"
-		aria-label="See our specials">See our specials</a
-	>
-</button>
+<!-- Specials button (on desktop) -->
+<a
+	href="menu/specials"
+	class="hidden w-full p-6 m-6 text-3xl font-thin text-center -translate-y-12 md:block hover:text-gray-300"
+	aria-label="See our specials"
+>
+	See our specials
+</a>
 
-<!-- Menu nav -->
-<nav class="flex flex-col items-center justify-center w-2/3 m-12 text-2xl font-thin md:hidden">
-	<a href="/menu/apps" class="p-6" aria-label="Appetizers">Appetizers</a>
-	<a href="/menu/entrees" class="p-6" aria-label="Entrees">Entrees</a>
-	<a href="/menu/burgers" class="p-6" aria-label="Burgers">Burgers</a>
-	<a href="/menu/sandwiches" class="p-6" aria-label="Sandwiches">Sandwiches</a>
-	<a href="/menu/kids" class="p-6" aria-label="Kids">Kids'</a>
-	<a href="/menu/dessert" class="p-6" aria-label="Dessert">Dessert</a>
+<!-- Menu navigation (on mobile) -->
+<nav class="flex flex-col items-center justify-center w-full m-12 text-2xl font-thin md:hidden">
+	<a
+		href="/menu/apps"
+		class="block w-full p-6 text-center hover:opacity-85"
+		aria-label="Appetizers"
+	>
+		Appetizers
+	</a>
+	<a
+		href="/menu/entrees"
+		class="block w-full p-6 text-center hover:opacity-85"
+		aria-label="Entrees"
+	>
+		Entrees
+	</a>
+	<a
+		href="/menu/burgers"
+		class="block w-full p-6 text-center hover:opacity-85"
+		aria-label="Burgers"
+	>
+		Burgers
+	</a>
+	<a
+		href="/menu/sandwiches"
+		class="block w-full p-6 text-center hover:opacity-85"
+		aria-label="Sandwiches"
+	>
+		Sandwiches
+	</a>
+	<a href="/menu/kids" class="block w-full p-6 text-center hover:opacity-85" aria-label="Kids">
+		Kids'
+	</a>
+	<a
+		href="/menu/dessert"
+		class="block w-full p-6 text-center hover:opacity-85"
+		aria-label="Dessert"
+	>
+		Dessert
+	</a>
 	<a
 		href="menu/specials"
-		class="p-6 text-2xl font-thin hover:text-gray-300"
-		aria-label="See our specials">Specials</a
+		class="block w-full p-6 text-2xl font-thin text-center hover:text-gray-300"
+		aria-label="See our specials"
 	>
+		Specials
+	</a>
 </nav>

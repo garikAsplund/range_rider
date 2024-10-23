01ad0fade09fd85c27040c86f383be6e4fd04e68
Garik Asplund <garik.asplund@gmail.com>
2024-10-23 15:53:10 -0700

    adding seo for bands

--- src/lib/bands.ts
+++ src/lib/bands.ts
@@ -1,13 +1,14 @@
 export type Band = {
     name: string;
     date: string;
+    isoDate: string;
     time: string;
     link: string;
   };
   
   export const bands: Array<Band> = [
-    { name: 'Bart Budwig', date: 'Fri 8 Nov', time: '8 pm', link: 'https://www.bartbudwig.com/' },
-    { name: 'Margo Cilker', date: 'Thu 14 Nov', time: '7 pm', link: 'https://www.margocilkermusic.com/' },
-    { name: 'The Shook Twins', date: 'Sat 23 Nov', time: '9 pm', link: 'https://shooktwins.com/' }
+    { name: 'Bart Budwig', date: 'Fri 8 Nov', isoDate: '2024-11-08T20:00:00-08:00', time: '8 pm', link: 'https://www.bartbudwig.com/' },
+    { name: 'Margo Cilker', date: 'Thu 14 Nov', isoDate: '2024-11-14T19:00:00-08:00', time: '7 pm', link: 'https://www.margocilkermusic.com/' },
+    { name: 'The Shook Twins', date: 'Sat 23 Nov', isoDate: '2024-11-23T21:00:00-08:00', time: '9 pm', link: 'https://shooktwins.com/' }
   ];
   
--- src/lib/components/BandInfo.svelte
+++ src/lib/components/BandInfo.svelte
@@ -1,18 +1,53 @@
 <script lang="ts">
 	import { bands } from '$lib/bands';
+
+	function getStructuredData(bands) {
+		return JSON.stringify(
+			bands.map((band) => ({
+				'@context': 'https://schema.org',
+				'@type': 'Event',
+				name: band.name,
+				startDate: band.isoDate,
+				location: {
+					'@type': 'Place',
+					name: 'The Range Rider',
+					address: {
+						'@type': 'PostalAddress',
+						streetAddress: '107 NW 1st St',
+						addressLocality: 'Enterprise',
+						addressRegion: 'Oregon',
+						postalCode: '97828',
+						addressCountry: 'US'
+					}
+				},
+				performer: {
+					'@type': 'MusicGroup',
+					name: band.name
+				},
+				url: band.link
+			}))
+		);
+	}
+
+	const structuredData = getStructuredData(bands);
 </script>
 
+<svelte:head>
+	{@html '<script type="application/ld+json">' + structuredData + '</script>'}
+
+</svelte:head>
+
 <div class="flex flex-col items-center justify-center w-full max-w-md px-4 md:-translate-y-12">
-    {#each bands as band}
-      <div class="flex flex-col w-full space-y-2 text-center">
-        <div class="text-xl font-thin">{band.date}</div>
-        <a href={band.link} class="text-lg hover:text-gray-300" target="_blank" rel="noopener">
-          {band.name}
-        </a>
-        <div class="font-thin text-normal">{band.time}</div>
-      </div>
-      {#if band !== bands[bands.length - 1]}
-        <hr class="w-2/3 my-4 border-t border-gray-500" />
-      {/if}
-    {/each}
-  </div>
+	{#each bands as band}
+		<div class="flex flex-col w-full space-y-2 text-center">
+			<div class="text-xl font-thin">{band.date}</div>
+			<a href={band.link} class="text-lg hover:text-gray-300" target="_blank" rel="noopener">
+				{band.name}
+			</a>
+			<div class="font-thin text-normal">{band.time}</div>
+		</div>
+		{#if band !== bands[bands.length - 1]}
+			<hr class="w-2/3 my-4 border-t border-gray-500" />
+		{/if}
+	{/each}
+</div>
